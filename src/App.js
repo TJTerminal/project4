6868bc0a68ff96731be4705dde6a9b911196b556
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-25 15:36:52 -0700

    login function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import SignupPage from './pages/SignupPage/SignupPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello this is App.js!
+      <Link to="/signup">
+        SignUp
+      </Link>
+
+      <Link to="/login">
+        LogIn
+      </Link>
+
+      <Switch>
+        <Route path="/signup" component={() => 
+        <SignupPage />
+        } />
+        <Route path="/login" component={() => 
+        <LoginPage />
+        }/>
+      </Switch>
     </div>
   );
 }
