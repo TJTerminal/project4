a19c59de3b737fa8abd12b4df78959baddf5cca8
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-26 16:34:28 -0700

    step 4

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
+
 import SignupPage from '../SignupPage/SignupPage';
 import LoginPage from '../LoginPage/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      Hello this is App.js!
-      <Link to="/signup">
-        SignUp
-      </Link>
+import userService from '../../utils/userService';
 
-      <Link to="/login">
-        LogIn
-      </Link>
 
-      <Switch>
-        <Route path="/signup" component={() => 
-        <SignupPage />
-        } />
-        <Route path="/login" component={() => 
-        <LoginPage />
-        }/>
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: userService.getUser(),
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        Hello this is App.js!
+        <Link to="/signup">
+          SignUp
+        </Link>
+
+        <Link to="/login">
+          LogIn
+        </Link>
+
+        <Switch>
+          <Route path="/signup" component={() => 
+          <SignupPage />
+          } />
+          <Route path="/login" component={() => 
+          <LoginPage />
+          }/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
