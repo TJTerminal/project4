853a22c311edb4dbef1809495e424fbfe55271b3
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-29 14:03:58 -0700

    added delete and update function to factService

--- src/utils/factService.js
+++ src/utils/factService.js
@@ -1,19 +1,40 @@
+import tokenService from './tokenService';
+
 const BASE_URL = '/api/facts/';
 
 function addFact(newFact) {
-    return fetch(BASE_URL,  {
+    return fetch(BASE_URL, {
       method: 'POST',
-      headers: new Headers({'Content-Type': 'application/json'}),
+      headers: {
+          'Content-Type': 'application/json', 
+          'Authorization': 'Bearer ' + tokenService.getToken()
+        },
       body: JSON.stringify(newFact)
-    }).then( res => {
-        if (res.ok) {
-            return res.json()
+    }).then( res => res.json())
+}
+
+function deleteOne(id) {
+    return fetch(BASE_URL + id, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': 'Bearer ' + tokenService.getToken()
         }
-    } ).then( data => {
-        console.log(data, 'this is our new data')
-    })
+    }).then(res => res.json());
+}
+
+function update(newFact) {
+    return fetch(BASE_URL + newFact._id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json', 
+            'Authorization': 'Bearer ' + tokenService.getToken()
+        },
+        body: JSON.stringify(newFact)
+    }).then(res => res.json());
 }
 
 export default {
-    addFact
+    addFact,
+    deleteOne,
+    update
 }
