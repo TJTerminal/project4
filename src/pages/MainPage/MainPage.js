69d2e1208cc6b5dee54759828be978508a4c8dc9
Ted Jang <tedjang@Teds-MacBook-Pro.local>
2020-05-30 22:15:51 -0700

    changed MainPage.js

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import NavBar from '../../components/NavBar/NavBar';
-import AddFactPage from '../AddFactPage/AddFactPage';
-import userService from '../../utils/userService';
-import factService from '../../utils/factService';
+// import NavBar from '../../components/NavBar/NavBar';
+// import AddFactPage from '../AddFactPage/AddFactPage';
+// import userService from '../../utils/userService';
+// import factService from '../../utils/factService';
 
-class MainPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            user: userService.getUser(),
-            newFact: '',
-        }
-    }
+import addFactForm from '../../components/AddFactForm/AddFactForm';
 
-    async componentDidMount() {
-        const newFact = await factService.index();
-        this.setState({ newFact: newFact })
-    }
-    
-    render() {
-        return(
-            <div className="MainPage">
-               {this.state.newFact.map((fact, idx) => (
-                   
-                   <AddFactPage 
-                    handleSubmit={this.props.handleSubmit} 
-                    handleChange={this.props.handleChange}
-                    key={idx}
+function MainPage(props) {
+    return (
+        <div>
+            {props.newFact.map(fact => 
+                fact.owner===props.user._id ?
+                <addFactForm 
+                    key={fact._id}
                     fact={fact}
-                    />
-               ) )}
-                
-                <h1>This is MainPage!</h1>
-            </div>
-        )
-    }
+                    handleDeleteFact={props.handleDeleteFact}
+                />
+                )}
+        </div>
+    )
 }
+
+
+// class MainPage extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             user: userService.getUser(),
+//             newFact: '',
+//         }
+//     }
+
+//     async componentDidMount() {
+//         const newFact = await factService.index();
+//         this.setState({ newFact: newFact })
+//     }
+    
+//     render() {
+//         return(
+//             <div className="MainPage">
+//                {this.state.newFact.map((fact, idx) => (
+                   
+//                    <AddFactPage 
+//                     handleSubmit={this.props.handleSubmit} 
+//                     handleChange={this.props.handleChange}
+//                     key={idx}
+//                     fact={fact}
+//                     />
+//                ) )}
+                
+//                 <h1>This is MainPage!</h1>
+//             </div>
+//         )
+//     }
+// }
 
 export default MainPage;
